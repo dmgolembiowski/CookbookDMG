1644595577608978ed085ecec271d588536a3e69
David M. Golembiowski <38993137+dmgolembiowski@users.noreply.github.com>
2018-11-11 21:33:31 -0500

    Create test.py

---
+++ SELENIUM/test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        # Just opens the browser
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+    
+"""                                                                                                                                                                            2010 $ ./test.py
+.
+----------------------------------------------------------------------
+Ran 1 test in 4.793s
+
+OK
+"""
